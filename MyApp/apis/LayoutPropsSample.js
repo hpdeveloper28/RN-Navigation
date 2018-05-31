3c0e7c13e3470e2e17a64e088e270e994cddae48
hiren <hpatel@mobiquityinc.com>
2018-05-31 16:11:05 +0530

    Layout props with left, top, righ, bottom params

--- MyApp/apis/LayoutPropsSample.js
+++ MyApp/apis/LayoutPropsSample.js
@@ -11,48 +11,61 @@ export default class LayoutPropsSample extends React.Component {
         return (
             <View style={styles.container}>
                 <View style={styles.viewOne} >
-                    <Text style={styles.textOne}>
-                        This is first text
+                    <Text style={styles.childTextCenter}>
+                        1
                 </Text>
-                    <View style={styles.dividerView} />
-                    <Text style={styles.textOne}>
-                        This is second text
+                    <View style={styles.dividerViewHorizontal} />
+                    <Text style={styles.childTextCenter}>
+                        2
                 </Text>
                 </View>
-                <View style={styles.viewTwo} />
-                <View style={styles.viewThree} />
-                <View style={styles.viewFour} />
+                <View style={styles.viewTwo}>
+                    <Text style={styles.childTextCenter}>
+                        1
+                </Text>
+                    <View style={styles.dividerViewVertical} />
+                    <Text style={styles.childTextCenter}>
+                        2
+                </Text>
+                </View>
+                <View style={styles.viewThree} >
+                    <Text style={styles.childTextTop}>
+                        1
+                </Text>
+                    <View style={styles.dividerViewHorizontal} />
+                    <Text style={styles.childTextBottom}>
+                        2
+                </Text>
+                </View>
+                <View style={styles.viewFour} >
+                    <Text style={styles.childTextLeft}>
+                        1
+                </Text>
+                    <View style={styles.dividerViewHorizontal} />
+                    <Text style={styles.childTextRight}>
+                        2
+                </Text>
+                </View>
             </View>
         )
     }
 }
-``
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: 'column'
     },
     viewOne: {
-        flex: .3,
+        flex: .4,
         backgroundColor: '#FF5733',
-        alignItems: 'center',
-        flexDirection: 'column'
-    },
-    textOne: {
-        flex: 1,
-        color: 'white',
-        fontStyle: 'italic',
-        fontWeight: 'bold',
-        textAlign: "center",
-        textAlignVertical: "center",
-        textAlignHorizontal: "center",
-    }, dividerView: {
-        flex: .05,
-        backgroundColor: '#FFFFFF',
-        justifyContent: 'center'
+        flexDirection: 'column',
+        justifyContent: 'center',
     },
     viewTwo: {
         flex: .25,
+        flexDirection: 'row',
+        justifyContent: 'center',
         backgroundColor: '#32CAA5'
     },
     viewThree: {
@@ -60,7 +73,63 @@ const styles = StyleSheet.create({
         backgroundColor: '#5067DE'
     },
     viewFour: {
-        flex: .3,
+        flex: .2,
         backgroundColor: '#DE9950'
+    },
+    childTextCenter: {
+        flex: 1,
+        fontSize: 20,
+        color: 'white',
+        fontStyle: 'italic',
+        fontWeight: 'bold',
+        textAlign: "center",
+        textAlignVertical: "center",
+        textAlignHorizontal: "center",
+    },
+    childTextTop: {
+        flex: 1,
+        fontSize: 20,
+        color: 'white',
+        fontStyle: 'italic',
+        fontWeight: 'bold',
+        textAlign: "center",
+        textAlignVertical: "top"
+    },
+    childTextBottom: {
+        flex: 1,
+        fontSize: 20,
+        color: 'white',
+        fontStyle: 'italic',
+        fontWeight: 'bold',
+        textAlign: "center",
+        textAlignVertical: "bottom"
+    },
+    childTextLeft: {
+        flex: 1,
+        fontSize: 20,
+        color: 'white',
+        fontStyle: 'italic',
+        fontWeight: 'bold',
+        textAlign: "left",
+        textAlignVertical: "center"
+    },
+    childTextRight: {
+        flex: 1,
+        fontSize: 20,
+        color: 'white',
+        fontStyle: 'italic',
+        fontWeight: 'bold',
+        textAlign: "right",
+        textAlignVertical: "center"
+    },
+    dividerViewHorizontal: {
+        flex: 0.1,
+        backgroundColor: '#FFFFFF',
+        justifyContent: 'center'
+    },
+    dividerViewVertical: {
+        flex: 0.05,
+        backgroundColor: '#FFFFFF',
+        justifyContent: 'center'
     }
 })
